288278ae1417d3892d8ca8916eb861728d93bd6b
a9921 <cy63161@gmail.com>
2026-08-25 11:09:23 +0800

    更新rack_door模組版本至0.3，新增中斷處理函數，改善日誌輸出與錯誤處理

--- rack_door.c
+++ rack_door.c
@@ -4,11 +4,12 @@
 #include <linux/gpio/consumer.h>
 #include <linux/gpio/driver.h>
 #include <linux/err.h>
+#include <linux/interrupt.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("YUE");
 MODULE_DESCRIPTION("Rack door reed switch monitor (GPIO IRQ + debounce + chardev)");
-MODULE_VERSION("0.2");
+MODULE_VERSION("0.3");
 
 #define GPIO_CHIP_LABEL "pinctrl-rp1"
 #define DEV_NAME "rack_door1"
@@ -18,53 +19,60 @@ MODULE_VERSION("0.2");
 static struct gpio_device *gdev;
 static struct gpio_desc *door_desc;
 
-
+static int door_irq = -1;
+static irqreturn_t door_isr(int irq, void *dev_id)
+{
+  int val;
+  val = gpiod_get_value(door_desc);
+  if(val < 0)
+  {
+    pr_err("rack: 電位讀取失敗\n");
+    gpio_device_put(gdev);
+    return val;
+  }
+  pr_info("rack: 讀取GPIO: %d, val為:%d %s\n", DOOR_GPIO, val, val?"門開":"門關");
+  
+  return IRQ_HANDLED;
+}
 
 
 static int __init door_init(void)
 {
-  pr_info("door_init insmod\n");
+  pr_info("rack: door_init insmod\n");
   
-  int ret = gpiod_direction_input(door_desc);
-  int val = gpiod_get_value(door_desc);
-
+  int ret;
+  
   gdev = gpio_device_find_by_label(GPIO_CHIP_LABEL);
   if(!gdev)
   {
-    pr_err("GPIO控制器(rp1)找不到\n");
+    pr_err("rack: GPIO控制器(rp1)找不到\n");
     return -ENODEV;
   }
-
+  
   door_desc = gpio_device_get_desc(gdev, DOOR_GPIO);
   if(IS_ERR(door_desc))
   {
-    pr_err("%d找不到\n",DOOR_GPIO);
+    pr_err("rack: %d找不到\n",DOOR_GPIO);
     gpio_device_put(gdev);
     return PTR_ERR(door_desc);
   }
-
+  
+  ret = gpiod_direction_input(door_desc);
   if(ret)
   {
-    pr_err("GPIO設成輸入失敗\n");
+    pr_err("rack: GPIO設成輸入失敗\n");
     gpio_device_put(gdev);
     return ret;
   }
+  
 
-  if(val < 0)
-  {
-    pr_err("電位讀取失敗\n");
-    gpio_device_put(gdev);
-    return val;
-  }
-
-  pr_info("讀取GPIO: %u, val為: %d \n", door_desc, val);
   return 0;
 }
 
 static void __exit door_exit(void)
 {
   gpio_device_put(gdev);
-  pr_info("door_exit rmmod\n");
+  pr_info("rack: door_exit rmmod\n");
 }
 
 module_init(door_init);
